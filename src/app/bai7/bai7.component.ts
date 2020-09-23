a8f5dce537cc7a89d8959364bc8b28bca241ea5d
chubin1999 <phachgia1999@gmail.com>
2020-09-23 16:42:55 +0700

    upload

--- src/app/bai7/bai7.component.ts
+++ src/app/bai7/bai7.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bai7',
-  templateUrl: './bai7.component.html',
+  templateUrl:'./bai7.component.html',
   styleUrls: ['./bai7.component.css']
 })
 export class Bai7Component implements OnInit {
